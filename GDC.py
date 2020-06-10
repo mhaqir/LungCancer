da3a799e6b370e2c888bf55b4f5da176a2f90b39
Mohammad  Haghir Ebrahimabadi <haghirebrahimm2@biowulf.nih.gov>
2020-06-10 11:10:00 -0400

    GDC

--- GDC.py
+++ GDC.py
@@ -3,10 +3,10 @@
 
 import pandas as pd
 import numpy as np
-
+from collections import Counter
 
 store = pd.HDFStore('GDC_samples_mutations.h5')
-samples_mutations_load = store['samples_mutations']
+samples_mutations_load = store['GDC_samples_mutations']
 
 
 print(np.sum(samples_mutations_load.values))
@@ -47,3 +47,18 @@ print(len(samples_mutations_load.columns))
 print((np.sum(samples_mutations_load.values == 1))/(np.shape(samples_mutations_load.values)[0] * np.shape(samples_mutations_load.values)[1]))
 
 print(samples_mutations_load.values[0:5, 0:5])
+
+
+
+
+with open('GDC_samples.txt', 'r') as f:
+	lines = f.readlines()
+
+
+samples = [s.rstrip().split('	') for s in lines[1:]]
+
+samples = np.asarray(samples)
+
+print(Counter(list(samples[:, 3])))
+
+print(Counter(list(samples[:, 4])))
